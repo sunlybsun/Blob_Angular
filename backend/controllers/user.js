9eb0bbc0522b4e21c192669b6808462c247e43de
sunlybsun <yanbo525@gmail.com>
2021-03-14 16:01:53 -0400

    master

---
+++ backend/controllers/user.js
@@ -0,0 +1,66 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+exports.createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    });
+    user
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: "User created!",
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "Invalid authentication credentials!"
+        });
+      });
+  });
+}
+
+exports.userLogin = (req, res, next) => {
+  console.log(111)
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      console.log(222)
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+
+    })
+    .then(result => {
+      console.log(333)
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "secret_this_should_be_longer",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token,
+        expiresIn: 3600,
+        userId: fetchedUser._id
+      });
+    })
+    .catch(err => {
+      return res.status(402).json({
+        message: "Invalid authentication credentials!"
+      });
+    });
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+import { HeaderComponent } from './header/header.component';import { PostListComponent } from './posts/post-list/post-list.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from "./auth/auth-interceptor";
+import { AngularMaterialModule } from "./angular-material.module";
+import { PostsModule } from "./posts/posts.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+
+    HttpClientModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    PostsModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
